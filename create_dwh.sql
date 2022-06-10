8c5d476d6edc6ec9e6adf47412831d381d56f98a
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2022-06-10 19:52:34 +0200

    update dwh creation script

--- create_dwh.sql
+++ create_dwh.sql
@@ -5,102 +5,113 @@ GO
 
 CREATE TABLE [dbo].[DateDim] (
   [DateID] INT PRIMARY KEY IDENTITY(1, 1),
-  [Date] DATE,
-  [Day] TINYINT,
-  [DaySuffix] CHAR(2),
-  [Weekday] TINYINT,
-  [WeekDayName] VARCHAR(10),
-  [IsHoliday] BIT,
-  [IsWeekend] BIT,
+  [Date] DATE NOT NULL,
+  [Day] TINYINT NOT NULL,
+  [DaySuffix] CHAR(2)NOT NULL,
+  [Weekday] TINYINT NOT NULL,
+  [WeekDayName] VARCHAR(10)NOT NULL,
+  [IsHoliday] BIT NOT NULL,
+  [IsWeekend] BIT NOT NULL,
   [HolidayText] VARCHAR(64),
-  [DOWInMonth] TINYINT,
-  [DayOfYear] SMALLINT,
-  [WeekOfMonth] TINYINT,
-  [WeekOfYear] TINYINT,
-  [ISOWeekOfYear] TINYINT,
-  [Month] TINYINT,
-  [MonthName] VARCHAR(10),
-  [Quarter] TINYINT,
-  [QuarterName] VARCHAR(6),
-  [Year] INT,
-  [MMYYYY] CHAR(6),
-  [MonthYear] CHAR(7),
-  [FirstDayOfMonth] DATE,
-  [LastDayOfMonth] DATE,
-  [FirstDayOfQuarter] DATE,
-  [LastDayOfQuarter] DATE,
-  [FirstDayOfYear] DATE,
-  [LastDayOfYear] DATE,
-  [FirstDayOfNextMonth] DATE,
-  [FirstDayOfNextYear] DATE
+  [DOWInMonth] TINYINT NOT NULL,
+  [DayOfYear] SMALLINT NOT NULL,
+  [WeekOfMonth] TINYINT NOT NULL,
+  [WeekOfYear] TINYINT NOT NULL,
+  [ISOWeekOfYear] TINYINT NOT NULL,
+  [Month] TINYINT NOT NULL,
+  [MonthName] VARCHAR(10) NOT NULL,
+  [Quarter] TINYINT NOT NULL,
+  [QuarterName] VARCHAR(6) NOT NULL,
+  [Year] INT NOT NULL,
+  [MMYYYY] CHAR(6) NOT NULL,
+  [MonthYear] CHAR(7) NOT NULL,
+  [FirstDayOfMonth] DATE NOT NULL,
+  [LastDayOfMonth] DATE NOT NULL,
+  [FirstDayOfQuarter] DATE NOT NULL,
+  [LastDayOfQuarter] DATE NOT NULL,
+  [FirstDayOfYear] DATE NOT NULL,
+  [LastDayOfYear] DATE NOT NULL,
+  [FirstDayOfNextMonth] DATE NOT NULL,
+  [FirstDayOfNextYear] DATE NOT NULL
 )
 GO
 
 CREATE TABLE [dbo].[CustomerDim] (
-  [CustomerID] int PRIMARY KEY IDENTITY(1, 1),
+  [CustomerSKID] int PRIMARY KEY IDENTITY(1, 1),
+  [CustomerID] int NOT NULL,
   [CustomerName] VARCHAR(60) NOT NULL,
-  [Phone] VARCHAR(12) NOT NULL,
-  [AdressLine1] VARCHAR(30) NOT NULL,
-  [AdressLine2] VARCHAR(30),
-  [ContactLastName] VARCHAR(20) NOT NULL,
-  [ContactFirstName] VARCHAR(20) NOT NULL
-)
-GO
-
-CREATE TABLE [dbo].[ProductDim] (
-  [ProductID] int PRIMARY KEY IDENTITY(1, 1),
-  [ProductLine] VARCHAR(30) NOT NULL,
-  [MSRP] int NOT NULL,
-  [PRODUCTCODE] VARCHAR(10) NOT NULL
-)
-GO
-
-CREATE TABLE [dbo].[GeographyDim] (
-  [GeographyID] int PRIMARY KEY IDENTITY(1, 1),
+  [ContactName] VARCHAR(20) NOT NULL,
   [City] VARCHAR(30) NOT NULL,
   [State] VARCHAR(30) NOT NULL,
   [PostalCode] VARCHAR(10),
   [Country] VARCHAR(20) NOT NULL,
   [Territory] VARCHAR(10) NOT NULL,
-  [Currency] CHAR(3) NOT NULL
+  [ValidFrom] DATE NOT NULL,
+  [ValidTo] DATE NOT NULL,
+  [CurrentFlag] CHAR(4)
 )
 GO
+
+CREATE TABLE [dbo].[ProductDim] (
+  [ProductSKID] int PRIMARY KEY IDENTITY(1, 1),
+  [ProductID] int NOT NULL,
+  [ProductLine] VARCHAR(30) NOT NULL,
+  [ProductCode] VARCHAR(10) NOT NULL,
+  [SuggestedRetailPricePLN] int NOT NULL,
+  [ValidFrom] DATE NOT NULL,
+  [ValidTo] DATE NOT NULL,
+  [CurrentFlag] CHAR(4)
+)
+GO
+
 
 CREATE TABLE [dbo].[OrderFact] (
   [OrderFactID] int PRIMARY KEY IDENTITY(1, 1),
-  [CustomerID] int NOT NULL,
-  [GeographyID] int NOT NULL,
-  [ProductID] int NOT NULL,
-  [Quantity] int NOT NULL,
-  [PriceEach] float NOT NULL,
+  [OrderNumber] int NOT NULL,
   [OrderLineNumber] int NOT NULL,
   [OrderDateID] int NOT NULL,
+  [ProductSKID] int NOT NULL,
+  [CustomerSKID] int NOT NULL,
   [Status] varchar(10) NOT NULL,
-  [DealSize] varchar(6) NOT NULL
+  [DealSize] varchar(6) NOT NULL,
+  [Quantity] int NOT NULL,
+  [UnitPriceLocalCurrency] money NOT NULL,
+  [UnitPricePLN] money NOT NULL,
+  [TotalDueLocalCurrency] money NOT NULL,
+  [TotalDuePLN] money NOT NULL,
+  [LocalCurrencyCode] char(3) NOT NULL
 )
 GO
 
-CREATE TABLE [dbo].[RatesFacts] (
-  [Currency] CHAR(3) NOT NULL,
-  [Rate] float NOT NULL,
-  [DateID] int NOT NULL
+CREATE TABLE [dbo].[CurrencyExchangeRateFact] (
+  [SourceCurrencyKey] CHAR(3) NOT NULL,
+  [DestinationCurrencyKey] CHAR(3) NOT NULL,
+  [ExchangeRate] money NOT NULL,
+  [DateID] int NOT NULL,
+  PRIMARY KEY (SourceCurrencyKey, DestinationCurrencyKey, DateID)
 )
 GO
 
-ALTER TABLE [dbo].[OrderFact] ADD FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[CustomerDim] ([CustomerID])
+CREATE TABLE [dbo].[CurrencyDim] (
+  [CurrencyKey] CHAR(3) PRIMARY KEY IDENTITY(1, 1),
+  [CurrencyName] VARCHAR(30) NOT NULL
+)
 GO
 
-ALTER TABLE [dbo].[OrderFact] ADD FOREIGN KEY ([GeographyID]) REFERENCES [dbo].[GeographyDim] ([GeographyID])
+ALTER TABLE [dbo].[OrderFact] ADD FOREIGN KEY ([CustomerSKID]) REFERENCES [dbo].[CustomerDim] ([CustomerSKID])
 GO
 
-ALTER TABLE [dbo].[OrderFact] ADD FOREIGN KEY ([ProductID]) REFERENCES [dbo].[ProductDim] ([ProductID])
+ALTER TABLE [dbo].[OrderFact] ADD FOREIGN KEY ([ProductSKID]) REFERENCES [dbo].[ProductDim] ([ProductSKID])
 GO
 
 ALTER TABLE [dbo].[OrderFact] ADD FOREIGN KEY ([OrderDateID]) REFERENCES [dbo].[DateDim] ([DateID])
 GO
 
-ALTER TABLE [dbo].[RatesFacts] ADD FOREIGN KEY ([Currency]) REFERENCES [dbo].[GeographyDim] ([Currency])
+ALTER TABLE [dbo].[CurrencyExchangeRateFact] ADD FOREIGN KEY ([SourceCurrencyKey]) REFERENCES [dbo].[CurrencyDim] ([CurrencyKey])
 GO
 
-ALTER TABLE [dbo].[RatesFacts] ADD FOREIGN KEY ([DateID]) REFERENCES [dbo].[DateDim] ([DateID])
+ALTER TABLE [dbo].[CurrencyExchangeRateFact] ADD FOREIGN KEY ([DestinationCurrencyKey]) REFERENCES [dbo].[CurrencyDim] ([CurrencyKey])
+GO
+
+ALTER TABLE [dbo].[CurrencyExchangeRateFact] ADD FOREIGN KEY ([DateID]) REFERENCES [dbo].[DateDim] ([DateID])
 GO
