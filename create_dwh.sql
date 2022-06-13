2d05730dee0111a9d4437fac2b614fce8bde8a8b
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2022-06-13 09:23:32 +0200

    add sql to create database version generated from DWH

--- create_dwh.sql
+++ create_dwh.sql
@@ -1,115 +1,173 @@
-CREATE DATABASE sales_dwh
-GO;
-USE sales_dwh
+
+CREATE DATABASE [SalesDWH]
 GO
-
-CREATE TABLE [dbo].[DateDim] (
-  [DateID] INT PRIMARY KEY IDENTITY(1, 1),
-  [Date] DATE NOT NULL,
-  [Day] TINYINT NOT NULL,
-  [DaySuffix] CHAR(2)NOT NULL,
-  [Weekday] TINYINT NOT NULL,
-  [WeekDayName] VARCHAR(10)NOT NULL,
-  [IsHoliday] BIT NOT NULL,
-  [IsWeekend] BIT NOT NULL,
-  [HolidayText] VARCHAR(64),
-  [DOWInMonth] TINYINT NOT NULL,
-  [DayOfYear] SMALLINT NOT NULL,
-  [WeekOfMonth] TINYINT NOT NULL,
-  [WeekOfYear] TINYINT NOT NULL,
-  [ISOWeekOfYear] TINYINT NOT NULL,
-  [Month] TINYINT NOT NULL,
-  [MonthName] VARCHAR(10) NOT NULL,
-  [Quarter] TINYINT NOT NULL,
-  [QuarterName] VARCHAR(6) NOT NULL,
-  [Year] INT NOT NULL,
-  [MMYYYY] CHAR(6) NOT NULL,
-  [MonthYear] CHAR(7) NOT NULL,
-  [FirstDayOfMonth] DATE NOT NULL,
-  [LastDayOfMonth] DATE NOT NULL,
-  [FirstDayOfQuarter] DATE NOT NULL,
-  [LastDayOfQuarter] DATE NOT NULL,
-  [FirstDayOfYear] DATE NOT NULL,
-  [LastDayOfYear] DATE NOT NULL,
-  [FirstDayOfNextMonth] DATE NOT NULL,
-  [FirstDayOfNextYear] DATE NOT NULL
-)
+USE [SalesDWH]
 GO
-
-CREATE TABLE [dbo].[CustomerDim] (
-  [CustomerSKID] int PRIMARY KEY IDENTITY(1, 1),
-  [CustomerName] VARCHAR(60) NOT NULL,
-  [ContactName] VARCHAR(20) NOT NULL,
-  [City] VARCHAR(30) NOT NULL,
-  [State] VARCHAR(30) NOT NULL,
-  [PostalCode] VARCHAR(10),
-  [Country] VARCHAR(20) NOT NULL,
-  [Territory] VARCHAR(10) NOT NULL,
-  [ValidFrom] DATE NOT NULL,
-  [ValidTo] DATE NOT NULL,
-  [CurrentFlag] CHAR(4)
-)
+/****** Object:  Table [dbo].[CurrencyDim]    Script Date: 6/12/2022 10:27:05 PM ******/
+SET ANSI_NULLS ON
 GO
-
-CREATE TABLE [dbo].[ProductDim] (
-  [ProductSKID] int PRIMARY KEY IDENTITY(1, 1),
-  [ProductLine] VARCHAR(30) NOT NULL,
-  [ProductCode] VARCHAR(10) NOT NULL,
-  [SuggestedRetailPricePLN] int NOT NULL,
-  [ValidFrom] DATE NOT NULL,
-  [ValidTo] DATE NOT NULL,
-  [CurrentFlag] CHAR(4)
-)
+SET QUOTED_IDENTIFIER ON
 GO
-
-
-CREATE TABLE [dbo].[OrderFact] (
-  [OrderFactID] int PRIMARY KEY IDENTITY(1, 1),
-  [OrderNumber] int NOT NULL,
-  [OrderLineNumber] int NOT NULL,
-  [OrderDateID] int NOT NULL,
-  [ProductSKID] int NOT NULL,
-  [CustomerSKID] int NOT NULL,
-  [Status] varchar(10) NOT NULL,
-  [DealSize] varchar(6) NOT NULL,
-  [Quantity] int NOT NULL,
-  [UnitPriceLocalCurrency] money NOT NULL,
-  [UnitPricePLN] money NOT NULL,
-  [TotalDueLocalCurrency] money NOT NULL,
-  [TotalDuePLN] money NOT NULL,
-  [LocalCurrencyCode] char(3) NOT NULL
-)
+CREATE TABLE [dbo].[CurrencyDim](
+	[CurrencyKey] [char](3) NOT NULL,
+	[CurrencyName] [varchar](30) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[CurrencyKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
-
-CREATE TABLE [dbo].[CurrencyExchangeRateFact] (
-  [SourceCurrencyKey] CHAR(3) NOT NULL,
-  [DestinationCurrencyKey] CHAR(3) NOT NULL,
-  [ExchangeRate] money NOT NULL,
-  [DateID] int NOT NULL,
-  PRIMARY KEY (SourceCurrencyKey, DestinationCurrencyKey, DateID)
-)
+/****** Object:  Table [dbo].[CurrencyExchangeRateFact]    Script Date: 6/12/2022 10:27:05 PM ******/
+SET ANSI_NULLS ON
 GO
-
-CREATE TABLE [dbo].[CurrencyDim] (
-  [CurrencyKey] CHAR(3) PRIMARY KEY,
-  [CurrencyName] VARCHAR(30) NOT NULL
-)
+SET QUOTED_IDENTIFIER ON
 GO
-
-ALTER TABLE [dbo].[OrderFact] ADD FOREIGN KEY ([CustomerSKID]) REFERENCES [dbo].[CustomerDim] ([CustomerSKID])
+CREATE TABLE [dbo].[CurrencyExchangeRateFact](
+	[SourceCurrencyKey] [char](3) NOT NULL,
+	[DestinationCurrencyKey] [char](3) NOT NULL,
+	[FactSKID] [int] IDENTITY(1,1) NOT NULL,
+	[ExchangeRate] [money] NOT NULL,
+	[DateID] [int] NOT NULL,
+ CONSTRAINT [PK_CurrencyExchangeRateFact] PRIMARY KEY CLUSTERED 
+(
+	[FactSKID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
-
-ALTER TABLE [dbo].[OrderFact] ADD FOREIGN KEY ([ProductSKID]) REFERENCES [dbo].[ProductDim] ([ProductSKID])
+/****** Object:  Table [dbo].[CustomerDim]    Script Date: 6/12/2022 10:27:05 PM ******/
+SET ANSI_NULLS ON
 GO
-
-ALTER TABLE [dbo].[OrderFact] ADD FOREIGN KEY ([OrderDateID]) REFERENCES [dbo].[DateDim] ([DateID])
+SET QUOTED_IDENTIFIER ON
 GO
-
-ALTER TABLE [dbo].[CurrencyExchangeRateFact] ADD FOREIGN KEY ([SourceCurrencyKey]) REFERENCES [dbo].[CurrencyDim] ([CurrencyKey])
+CREATE TABLE [dbo].[CustomerDim](
+	[CustomerSKID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerName] [varchar](60) NOT NULL,
+	[ContactName] [varchar](51) NOT NULL,
+	[City] [varchar](30) NOT NULL,
+	[State] [varchar](30) NOT NULL,
+	[PostalCode] [varchar](10) NULL,
+	[Country] [varchar](20) NOT NULL,
+	[Territory] [varchar](10) NOT NULL,
+	[ValidFrom] [date] NOT NULL,
+	[ValidTo] [date] NOT NULL,
+	[CurrentFlag] [char](4) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[CustomerSKID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
-
-ALTER TABLE [dbo].[CurrencyExchangeRateFact] ADD FOREIGN KEY ([DestinationCurrencyKey]) REFERENCES [dbo].[CurrencyDim] ([CurrencyKey])
+/****** Object:  Table [dbo].[DateDimension]    Script Date: 6/12/2022 10:27:05 PM ******/
+SET ANSI_NULLS ON
 GO
-
-ALTER TABLE [dbo].[CurrencyExchangeRateFact] ADD FOREIGN KEY ([DateID]) REFERENCES [dbo].[DateDim] ([DateID])
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[DateDimension](
+	[DateID] [int] NOT NULL,
+	[Date] [date] NOT NULL,
+	[Day] [tinyint] NOT NULL,
+	[DaySuffix] [char](2) NOT NULL,
+	[Weekday] [tinyint] NOT NULL,
+	[WeekDayName] [varchar](10) NOT NULL,
+	[IsWeekend] [bit] NOT NULL,
+	[IsHoliday] [bit] NOT NULL,
+	[HolidayText] [varchar](64) SPARSE  NULL,
+	[DOWInMonth] [tinyint] NOT NULL,
+	[DayOfYear] [smallint] NOT NULL,
+	[WeekOfMonth] [tinyint] NOT NULL,
+	[WeekOfYear] [tinyint] NOT NULL,
+	[ISOWeekOfYear] [tinyint] NOT NULL,
+	[Month] [tinyint] NOT NULL,
+	[MonthName] [varchar](10) NOT NULL,
+	[Quarter] [tinyint] NOT NULL,
+	[QuarterName] [varchar](6) NOT NULL,
+	[Year] [int] NOT NULL,
+	[MMYYYY] [char](6) NOT NULL,
+	[MonthYear] [char](7) NOT NULL,
+	[FirstDayOfMonth] [date] NOT NULL,
+	[LastDayOfMonth] [date] NOT NULL,
+	[FirstDayOfQuarter] [date] NOT NULL,
+	[LastDayOfQuarter] [date] NOT NULL,
+	[FirstDayOfYear] [date] NOT NULL,
+	[LastDayOfYear] [date] NOT NULL,
+	[FirstDayOfNextMonth] [date] NOT NULL,
+	[FirstDayOfNextYear] [date] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[DateID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[OrderFact]    Script Date: 6/12/2022 10:27:05 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OrderFact](
+	[OrderFactID] [int] IDENTITY(1,1) NOT NULL,
+	[OrderNumber] [int] NOT NULL,
+	[OrderLineNumber] [int] NOT NULL,
+	[OrderDateID] [int] NOT NULL,
+	[ProductSKID] [int] NOT NULL,
+	[CustomerSKID] [int] NOT NULL,
+	[Status] [varchar](10) NOT NULL,
+	[DealSize] [varchar](6) NOT NULL,
+	[Quantity] [int] NOT NULL,
+	[UnitPriceLocalCurrency] [money] NOT NULL,
+	[UnitPricePLN] [money] NOT NULL,
+	[TotalDueLocalCurrency] [money] NOT NULL,
+	[TotalDuePLN] [money] NOT NULL,
+	[LocalCurrencyCode] [char](3) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[OrderFactID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[ProductDim]    Script Date: 6/12/2022 10:27:05 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ProductDim](
+	[ProductSKID] [int] IDENTITY(1,1) NOT NULL,
+	[ProductLine] [varchar](30) NOT NULL,
+	[ProductCode] [varchar](10) NOT NULL,
+	[SuggestedRetailPricePLN] [int] NOT NULL,
+	[ValidFrom] [date] NOT NULL,
+	[ValidTo] [date] NOT NULL,
+	[CurrentFlag] [char](4) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ProductSKID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[CurrencyExchangeRateFact]  WITH CHECK ADD  CONSTRAINT [FK__CurrencyE__Sourc__4D94879B] FOREIGN KEY([SourceCurrencyKey])
+REFERENCES [dbo].[CurrencyDim] ([CurrencyKey])
+GO
+ALTER TABLE [dbo].[CurrencyExchangeRateFact] CHECK CONSTRAINT [FK__CurrencyE__Sourc__4D94879B]
+GO
+ALTER TABLE [dbo].[CurrencyExchangeRateFact]  WITH CHECK ADD  CONSTRAINT [FK_CurrencyExchangeRateFact_DateDimension] FOREIGN KEY([DateID])
+REFERENCES [dbo].[DateDimension] ([DateID])
+GO
+ALTER TABLE [dbo].[CurrencyExchangeRateFact] CHECK CONSTRAINT [FK_CurrencyExchangeRateFact_DateDimension]
+GO
+ALTER TABLE [dbo].[OrderFact]  WITH CHECK ADD FOREIGN KEY([ProductSKID])
+REFERENCES [dbo].[ProductDim] ([ProductSKID])
+GO
+ALTER TABLE [dbo].[OrderFact]  WITH CHECK ADD  CONSTRAINT [FK_OrderFact_CurrencyDim] FOREIGN KEY([LocalCurrencyCode])
+REFERENCES [dbo].[CurrencyDim] ([CurrencyKey])
+GO
+ALTER TABLE [dbo].[OrderFact] CHECK CONSTRAINT [FK_OrderFact_CurrencyDim]
+GO
+ALTER TABLE [dbo].[OrderFact]  WITH CHECK ADD  CONSTRAINT [FK_OrderFact_CustomerDim] FOREIGN KEY([CustomerSKID])
+REFERENCES [dbo].[CustomerDim] ([CustomerSKID])
+GO
+ALTER TABLE [dbo].[OrderFact] CHECK CONSTRAINT [FK_OrderFact_CustomerDim]
+GO
+ALTER TABLE [dbo].[OrderFact]  WITH CHECK ADD  CONSTRAINT [FK_OrderFact_DateDimension] FOREIGN KEY([OrderDateID])
+REFERENCES [dbo].[DateDimension] ([DateID])
+GO
+ALTER TABLE [dbo].[OrderFact] CHECK CONSTRAINT [FK_OrderFact_DateDimension]
 GO
