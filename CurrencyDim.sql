cb30b305d58138e7a0ea320efe7d36e72a3a411e
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2022-06-13 09:20:52 +0200

    add currency dim insertion

---
+++ CurrencyDim.sql
@@ -0,0 +1,13 @@
+INSERT INTO CurrencyDim (CurrencyKey, CurrencyName)
+VALUES ('AUD', 'Australian dollar'),
+('EUR', 'Euro'),
+('CAD', 'Canadian dollar'),
+('JPY', 'Japanese yen'),
+('NOK', 'Norwegian krone'),
+('PHP', 'Philippine peso'),
+('SGD', 'Singapore dollar'),
+('SEK', 'Swedish krona'),
+('CHF', 'Swiss franc'),
+('GBP', 'British pound'),
+('USD', 'United States dollar'),
+('PLN', 'Polish zloty')
